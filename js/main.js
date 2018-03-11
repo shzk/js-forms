ae0373da3bb300033eac7978820b56403cf21e94
Denis Tokarev <den4@me.com>
2018-03-11 21:30:59 +0300

    registration for validation ready
    also some minor improvements in main.js

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
 
     (function(){
 
-        var validateForm = {
+        var validateCommentForm = {
     
             init: function(){
                 this._setUpListeners();
             },
     
             _setUpListeners: function(){
-                $('#sendButton').on( 'click', validateForm._validateComment );
+                $('#sendButton').on( 'click', validateCommentForm._validateComment );
             },
 
             _validateComment: function(event){
@@ -42,20 +42,21 @@ $(document).ready(function(){
             }
         };
     
-        validateForm.init();
+        validateCommentForm.init();
     
     }());
-    // end of form validation
+    // end of comment form validation
 
     (function(){
-        var validateLogin = {
+
+        var validateLoginForm = {
     
             init: function(){
                 this._setUpListeners();
             },
     
             _setUpListeners: function(){
-                $('#loginButton').on('click', validateLogin._checkLogin );
+                $('#loginButton').on('click', validateLoginForm._checkLogin );
             },
     
             _checkLogin: function(event){
@@ -122,9 +123,7 @@ $(document).ready(function(){
 
                     //hide errors
                     input.on('focus keydown change', function(){
-                    errorClass = input.attr('type');
                     form.find('.error-email, .error-password').remove();
-
                     });
 
                     if ( emailValid === true && passwordValid === true) {
@@ -147,7 +146,107 @@ $(document).ready(function(){
             },
         };
     
-        validateLogin.init();
+        validateLoginForm.init();
+    
+    }());
+    // end of login form validation
+
+    (function(){
+
+        var validateRegistrationForm = {
+    
+            init: function(){
+                this._setUpListeners();
+            },
+    
+            _setUpListeners: function(){
+                $('#registrationButton').on('click', validateRegistrationForm._checkRegistration );
+            },
+    
+            _checkRegistration: function(event){
+                event.preventDefault();
+                var form = $('#registrationForm'),
+                    inputs = form.find('input'),
+                    heading = form.find('.plate__heading'),
+                    existingEmail = "mail@mail.com",
+                    emailValid,
+                    emailValue,
+                    passwordValid,
+                    passwordValue;
+
+                $.each( inputs, function(index, val) {
+                    var input = $(val),
+                        value = input.val().trim().toLowerCase(),
+                        errorClass = input.attr('type'),
+                        errorText = input.attr('data-empty-error'),
+                        errorTooltip = $('<div class="error error-' + errorClass +'">' + errorText + '</div>');
+
+                    if ( input.attr('type').toLowerCase() === 'email' ) {
+                        if ( value.length === 0 ) {
+                            form.find('.error-email').remove();
+                            input.after(errorTooltip);
+                            emailValid = false;
+                        } //end of checking value.length for empty email value
+                        if ( value !== '' ) {
+                            var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+                            if ( pattern.test(value) ) {
+                                form.find('.error-email').remove();
+                                emailValid = true;
+                                emailValue = value;
+                                console.log('Email is valid, value = ' + value);
+                            } else {
+                                form.find('.error-email').remove();
+                                errorText = input.attr('data-wrong-email');
+                                errorTooltip = $('<div class="error error-email">' + errorText + '</div>');
+                                input.after(errorTooltip);
+                                emailValid = false;
+                                emailValue = value;
+                                console.log('Email is INVALID, value = ' + value);
+                            } //end of testing email by pattern
+                        } //end of email value validation
+                    } //end of email validation
+
+                    if (input.attr('type').toLowerCase() === 'password' ) {
+                        if ( value.length === 0 ) {
+                            form.find('.error-password').remove();
+                            input.after(errorTooltip);
+                            passwordValid = false;
+                            console.log('password is INVALID, value is empty ');
+                        } //end of checking value.length for empty password value
+                        if ( value !== '' ) {
+                            form.find('.error-password').remove();
+                            passwordValid = true;
+                            passwordValue = value;
+                            console.log('password is valid, value =' + value);
+                        } //end of password validation
+                    } //end of password validation
+
+                    input.on('focus keydown change', function(){
+                        form.find('.error-email, .error-password').remove();
+                    }); //end of hiding errors
+
+                }); //end of $.each
+
+                if ( emailValid === true && passwordValid === true ) {
+                        console.log('form data is valid');
+                        console.log('email value is' + emailValue);
+                        if ( emailValue === existingEmail ) {
+                            form.find('.error-description, .error--with-desc').remove();
+                            errorText = 'Данный email уже занят';
+                            loginErrorText = '<p>Используйте другой email чтобы создать новый аккаунт.</p> \n <p>Или воспользуйтесь <a href="#">восстановлением пароля</a>, чтобы войти на сайт.</p>';
+                            loginErrorTooltip = '<div class="error-description">' + loginErrorText + '</div>';
+                            errorTooltip = $('<div class="error error--with-desc">' + errorText + '</div>' + loginErrorTooltip);
+                            heading.after(errorTooltip);
+                            console.log('sorry, email is already taken');
+                        } else {
+                            form.find('.error-description, .error--with-desc').remove();
+                            form.submit();
+                        }//end of checking for existing email address
+                    } //end of registration form validation
+            } //end of _checkRegistration
+        };
+    
+        validateRegistrationForm.init();
     
     }());
 
