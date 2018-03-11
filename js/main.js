e2db0879786ba52937985bf4c64f8870572b3975
Denis Tokarev <den4@me.com>
2018-03-11 13:52:17 +0300

    form Validation ready

--- js/main.js
+++ js/main.js
@@ -1,7 +1,48 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-	console.log('Hello world!');
+    (function(){
 
+        var validateForm = {
+    
+            init: function(){
+                this._setUpListeners();
+            },
+    
+            _setUpListeners: function(){
+                $('#sendButton').on( 'click', validateForm._validateComment );
+            },
+
+            _validateComment: function(event){
+                event.preventDefault();
+                //console.log('privateMethod _validateComment runs');
+                var valid = true,
+                    form = $('#commentForm'),
+                    commentArea = $('#commentArea'),
+                    commentText = commentArea.val().trim(),
+                    label = form.find('.label-title'),
+                    errorText = 'Комментарий не может быть пустым. Привет из js)',
+                    errorTooltip = $('<div class="error">' + errorText + '</div>');
+
+                if ( commentText.length === 0 ) {
+                    label.before(errorTooltip);
+                    valid = false;
+                } else {
+                    form.find('.error').remove();
+                }
+
+                commentArea.on('focus keydown change', function(){
+                    form.find('.error').remove();
+                    //console.log('we are now in textarea');
+                });
+
+                if (valid === true) {
+                    //console.log('everything is okay, sending form now');
+                    form.submit();
+                }
+            }
+        };
+    
+        validateForm.init();
+    
+    }());
 });
-
-
